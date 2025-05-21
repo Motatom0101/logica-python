6cc25aa15182da21a4690655353b0c12e4d3c17e
Tiago Mota <tmota.seg@gmail.com>
2025-05-21 16:37:39 -0300

    Aula14 - Aula 1 - Criação do 2º objeto

--- classe1.py
+++ classe1.py
@@ -15,3 +15,15 @@ print('Modelo: ',celular1.modelo)
 print('Cor: ',celular1.cor)
 print()
 celular1.email() #Chamando o método no objeto
+
+print('---------- Celular 2 ----------')
+print()
+
+celular2 = Smartphone() #Instanciando o 2º objeto
+celular2.marca = input('Digite a marca: ')
+celular2.modelo = input('Digite o modelo: ')
+celular2.cor = input('Digite a cor: ')
+print()
+print('Marca: ',celular2.marca)
+print('Modelo: ',celular2.modelo)
+print('Cor: ',celular2.cor)
